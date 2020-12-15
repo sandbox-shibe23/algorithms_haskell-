00c77fa7854c92badbd45bb5fe838bbe4bb35abb
shibe23 <shibe23.info@gmail.com>
2020-12-15 23:51:48 +0900

    Add SelectionSort

---
+++ sorts/SelectionSort.hs
@@ -0,0 +1,21 @@
+module Sorts.SelectionSort where
+
+listToSort :: [Int]
+listToSort = [13, 2, 3, 14, 17, 4, 1, 5, 16, 12, 9, 10, 15, 8, 7, 11, 18, 19, 6, 20]
+
+selectionSort :: (Ord a) => [a] -> [a]
+selectionSort [] = []
+selectionSort (x:xs) =
+  let (y, ys) = leastUnsorted (x:xs)
+  in y : selectionSort ys
+
+leastUnsorted :: (Ord a) => [a] -> (a, [a])
+leastUnsorted [x] = (x, [])
+leastUnsorted (x:xs) =
+  let (y, ys) = leastUnsorted xs
+  in if x <= y then (x, xs) else (y, x:ys)
+
+main = do
+  putStrLn $ "Unsorted: " ++ show listToSort
+  putStrLn $ "Sorted: " ++ show (selectionSort listToSort)
+
